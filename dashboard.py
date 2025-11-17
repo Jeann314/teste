791399f5427fc6b3f1ec6aa8bca552ec768f3a67
Jeann314 <jeanncbm@gmail.com>
2025-11-17 14:21:09 -0300

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -9,6 +9,17 @@ fig1 = px.line(df, x = 'Date_reported', y = 'Cumulative_cases', color = 'Country
 fig1.update_layout(xaxis_title = 'Data', yaxis_title = 'Número de casos acumulados', title_font_size = 30)
 fig1.show()
 
+df_brasil_usa = df.query('Country == "Brazil" or Country == "United States of America"')
+fig2 = px.line(df_brasil_usa, x = 'Date_reported', y = 'Cumulative_cases', color = 'Country', title='Comparativo entre os casos acumulados de Brasil e USA')
+fig2.update_layout(xaxis_title = 'Data', yaxis_title = 'Número de casos acumulados')
+fig2.show()
+
+df_brasil_usa_india = df.query('Country == "Brazil" or Country == "United States of America" or Country == "India"')
+fig3 = px.pie(df_brasil_usa_india, values = 'Cumulative_cases', names = 'Country', title = 'Comparativo entre os casos acumulados de Brasil, USA e Índia')
+fig3.show()
+
 st.title("DASHCOVID: Um Dashboard sobre os Dados de COVID-19 - 2020")
 
 st.plotly_chart(fig1, use_container_width = True)
+st.plotly_chart(fig2, use_container_width = True)
+st.plotly_chart(fig3, use_container_width = True)
