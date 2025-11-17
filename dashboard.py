3ef195744027ee14f220b69b4ada4b2fd0f61291
Jeann314 <jeanncbm@gmail.com>
2025-11-17 14:10:12 -0300

    dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-df = pd.read_csv('/content/drive/MyDrive/Trilha de Programação e Análise de Dados/Módulo 2 aula 4/WHO_time_series.csv')
+df = pd.read_csv('WHO_time_series.csv')
 
 fig1 = px.line(df, x = 'Date_reported', y = 'Cumulative_cases', color = 'Country',
                title = 'Dados de COVID-19 no mundo - Ano 2020')
